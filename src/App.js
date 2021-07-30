64e270dbc94d8864e89c1665c9b8f2e9384ff1ba
JeGwan <emperorvv@gmail.com>
2021-07-31 03:17:02 +0900

    feat: babel 추가 .browserslistrc 추가

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useCallback, useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const handlePlus = useCallback(() => setCount((prev) => prev + 1), []);
+  const handleMinus = useCallback(() => setCount((prev) => prev - 1), []);
+  return (
+    <div>
+      <div className="count">{count}</div>
+      <div className="buttons">
+        <button onClick={handlePlus}>Plus 😀</button>
+        <button onClick={handleMinus}>Minus 😡</button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
